cca6b5e89858473357c7f4220752409db42d5ad2
Issac038 <issac.antony@kalvium.community>
2025-01-15 10:25:40 +0530

    connect to mongoose and added a post and get req

--- index.js
+++ index.js
@@ -1,13 +1,60 @@
 const express = require('express');
-const { resolve } = require('path');
+const mongoose = require('mongoose')
+const MenuItem = require('./schema')
 
 const app = express();
-const port = 3010;
+const port = 3010
 
-app.use(express.static('static'));
+app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.sendFile(resolve(__dirname, 'pages/index.html'));
+mongoose
+  .connect("mongodb+srv://issac:Nemizez038@cluster0.zvt6a.mongodb.net/MenuItems")
+  .then(() => console.log("Connected to MongoDB Atlas"))
+  .catch((err) => console.error("MongoDB connection error:", err));
+
+app.post("/menu", async (req, res) => {
+  try {
+    const { name, description, price } = req.body;
+    if (!name || !price) {
+      return res.status(400).json({
+        success: false,
+        message: "Name and Price are required",
+      });
+    }
+
+    const newMenuItem = await MenuItem.create({
+      name,
+      description,
+      price,
+    });
+    res.status(201).json({
+      success: true,
+      message: "New menu item created successfully",
+      data: newMenuItem,
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: "Error creating menu item",
+      error: error.message,
+    });
+  }
+});
+
+app.get("/menu", async (req, res) => {
+  try {
+    const menuItems = await MenuItem.find();
+    res.status(200).json({
+      success: true,
+      data: menuItems,
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: "Error fetching menu items",
+      error: error.message,
+    });
+  }
 });
 
 app.listen(port, () => {
